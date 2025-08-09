2cc9bf57d7b2b9b8070d837ee86b7d24b5e4538a
Antwanola <antwanola29@gmail.com>
2025-08-09 17:23:16 +0100

    Adjusted mongo issues

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -1,8 +1,8 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
-export async function connectDB() {
-  await mongoose.connect('mongodb://localhost:27017/evms', {
-    serverSelectionTimeoutMS: 5000,
-  });
-  console.log('Connected to MongoDB');
-}
+// export async function connectDB() {
+//   await mongoose.connect('mongodb://localhost:27017/evms', {
+//     serverSelectionTimeoutMS: 5000,
+//   });
+//   console.log('Connected to MongoDB');
+// }
