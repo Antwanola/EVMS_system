df547700a2d24bfea39e1f06b73d7e942621eb30
Antwanola <antwanola29@gmail.com>
2025-10-02 12:48:28 +0100

    Allow vercel and localhost through cors

--- src/server.ts
+++ src/server.ts
@@ -38,7 +38,7 @@ class Application {
   private setupMiddleware(): void {
     this.app.use(helmet());
     this.app.use(cors({
-      origin: process.env.ckfjsdnj || "http://localhost:3001",
+      origin: ["https://evms-frontend-7p1a.vercel.app", "http://localhost:3000"],
       credentials: true
     }));
     this.app.use(express.json({ limit: '10mb' }));
